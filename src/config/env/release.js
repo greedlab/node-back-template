ca48b7797197955f8ffcd320b735de5986c96cdf
Bell <bell@greedlab.com>
2016-08-16 10:41:50 +0800

    save unvalid token to redis

---
+++ src/config/env/release.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Bell on 16/8/16.
+ */
+
+export default {
+    token: 'release-secret-jwt-token',
+    mongodb: 'mongodb://node:hungry97@node.greedlab.com/node-back-template',
+    redisOptions: {
+
+    }
+};
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,8 +4,8 @@
 
 import passport from 'koa-passport';
 import User from '../models/user';
-import UnvalidToken from '../models/unvalid-token';
-// import { addToken } from '../utils/unvalid-token';
+// import UnvalidToken from '../models/unvalid-token';
+import { addToken } from '../utils/unvalid-token';
 import { getToken } from '../utils/auth';
 
 /**
@@ -104,9 +104,9 @@ export async function logout(ctx, next) {
     try {
         const token = getToken(ctx);
         if (token) {
-            const unvalidToken = new UnvalidToken({token});
-            await unvalidToken.save();
-            // addToken(token);
+            // const unvalidToken = new UnvalidToken({token});
+            // await unvalidToken.save();
+            addToken(token);
         }
     } catch (err) {
         ctx.throw(422, err.message);
